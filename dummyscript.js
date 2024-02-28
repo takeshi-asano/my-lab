039e504b1815ed64a520be2acfa5e9f457cf203f
Takeshi Asano <takeshiasano14@gmail.com>
2024-02-29 01:12:11 +1300

    just add it

---
+++ dummyscript.js
@@ -0,0 +1,38 @@
+/* Count Vowels Kata */
+
+
+
+/* Step 1: Write your code here */
+
+function countVowels(word) {
+
+  
+  // Your code goes here
+  
+  //counts 
+  const vowels =  ["a", "e", "i", "o", "u"s]
+
+  return vowels;
+
+}
+
+
+
+/* Step 2: Write your code here to call your function and test that it works. Hint: you'll want a `console.log()` */
+
+let letters = 
+
+countVowels(letters)
+
+
+
+/* Step 3: To run the `countVowels` function, follow these instructions:
+  
+  1. Save the code for the `countVowels` function in a file named `countVowel.js` on your computer.
+  2. Open your terminal.
+  3. Navigate to the directory where the `countVowel.js` file is located using the `cd` command. For example, if the file is on your desktop, you can use the command `cd Desktop` to navigate to the desktop directory.
+  4. Once you are in the correct directory, type the following command in your terminal and press Enter: `node countVowel.js`
+  
+  The program will then count the number of vowels in the entered string and display the result in the terminal.
+  
+   */
